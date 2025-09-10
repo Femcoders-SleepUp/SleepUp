7c3d8eb45c610964013eafecd72295ce932a84a3
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-10 21:19:58 +0200

    feat: create AccommodationRepository and add findByManagedBy_Id method

---
+++ src/main/java/com/SleepUp/SU/accommodation/AccommodationRepository.java
@@ -0,0 +1,12 @@
+package com.SleepUp.SU.accommodation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository <Accommodation, Long>{
+    List<Accommodation> findByManagedBy_Id(Long userId);
+
+}
