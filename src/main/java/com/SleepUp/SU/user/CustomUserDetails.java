82a358263e2d5d0f370a163de1e81802095b72dc
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-10 22:31:13 +0200

    feat: add CustomUserDetails

--- src/main/java/com/SleepUp/SU/user/CustomUserDetails.java
+++ src/main/java/com/SleepUp/SU/user/CustomUserDetails.java
@@ -1,4 +1,46 @@
 package com.SleepUp.SU.user;
 
-public class CustomUserDetails {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (user.getRoles() == null) {
+            return List.of();
+        }
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
 }
