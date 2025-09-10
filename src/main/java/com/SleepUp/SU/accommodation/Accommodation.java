75ce278b74923278acfe71e9aadc0ff1892bf602
thaisrqueiroz <thaisrochadequeiroz@gmail.com>
2025-09-10 18:21:03 +0200

    feat: add Accommodation entity

---
+++ src/main/java/com/SleepUp/SU/accommodation/Accommodation.java
@@ -0,0 +1,52 @@
+package com.SleepUp.SU.accommodation;
+
+import com.SleepUp.SU.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "accommodations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false, name = "guest_number")
+    private int guestNumber;
+
+    @Column(nullable = false, length = 50)
+    private String location;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "check_in_time")
+    private LocalTime checkInTime;
+
+    @Column(name = "check_out_time")
+    private LocalTime checkOutTime;
+
+    @Column(nullable = false, name = "available_from")
+    private LocalDate availableFrom;
+
+    @Column(nullable = false, name = "available_to")
+    private LocalDate availableTo;
+
+    @ManyToOne
+    @JoinColumn(name = "managed_by_user_id")
+    private User managedBy;
+}
