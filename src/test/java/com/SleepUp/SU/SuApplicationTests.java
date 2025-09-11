ac2c335a74483e49f21ff4c01eb387fb4594f443
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-11 21:10:49 +0200

    ci: try new env.

--- src/test/java/com/SleepUp/SU/SuApplicationTests.java
+++ src/test/java/com/SleepUp/SU/SuApplicationTests.java
@@ -2,10 +2,12 @@ package com.SleepUp.SU;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@EnableScheduling
 class SuApplicationTests {
 
 	@Test
