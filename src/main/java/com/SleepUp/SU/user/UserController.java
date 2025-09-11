6c8b74da25cf2df0b34f6ec7cfed3fab58e8be80
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-11 10:05:54 +0200

    feat: add method to refresh token

--- src/main/java/com/SleepUp/SU/user/UserController.java
+++ src/main/java/com/SleepUp/SU/user/UserController.java
@@ -1,7 +1,9 @@
 package com.SleepUp.SU.user;
 
+import com.SleepUp.SU.auth.AuthServiceHelper;
 import com.SleepUp.SU.user.dto.USER.UserRequest;
 import com.SleepUp.SU.user.dto.UserResponse;
+import com.SleepUp.SU.utils.ApiMessageDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,6 +20,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private AuthServiceHelper authServiceHelper;
 
     @GetMapping
     public List<UserResponse> listAllUsers() {
@@ -34,6 +38,11 @@ public class UserController {
     public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRequest request) {
         UserResponse registeredUser = userService.registerUser(request);
         return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/auth/refresh")
+    public ResponseEntity<ApiMessageDto> refreshToken(@RequestBody String body) {
+        return authServiceHelper.handleRefreshToken(body);
     }
 }
 
