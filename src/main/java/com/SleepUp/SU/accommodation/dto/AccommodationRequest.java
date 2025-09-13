b95fe98a934acb0b283bf4749d9674501c921eb4
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-13 11:12:43 +0200

    feat: add imageUrl field to AccommodationRequest

--- src/main/java/com/SleepUp/SU/accommodation/dto/AccommodationRequest.java
+++ src/main/java/com/SleepUp/SU/accommodation/dto/AccommodationRequest.java
@@ -1,9 +1,6 @@
 package com.SleepUp.SU.accommodation.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -36,6 +33,9 @@ public record AccommodationRequest (
         LocalDate availableFrom,
 
         @NotNull
-        LocalDate availableTo
+        LocalDate availableTo,
+
+        @NotBlank
+        String imageUrl
 ){
 }
