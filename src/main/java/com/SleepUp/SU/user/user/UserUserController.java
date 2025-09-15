168d06878d8420127cba64d7a0edd4b9fdf1e677
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-15 15:09:24 +0200

    feature: add method for get logged user

--- src/main/java/com/SleepUp/SU/user/user/UserUserController.java
+++ src/main/java/com/SleepUp/SU/user/user/UserUserController.java
@@ -1,32 +1,25 @@
 package com.SleepUp.SU.user.user;
 
-import com.SleepUp.SU.user.admin.UserAdminService;
+import com.SleepUp.SU.user.CustomUserDetails;
 import com.SleepUp.SU.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserUserController {
 
-    private final UserAdminService userService;
+    private final UserUserService userUserService;
 
-    @GetMapping
-    public List<UserResponse> listAllUsers() {
-        return userService.getAllUsers();
+    @GetMapping("/my-user")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponse getLoggedUser(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        return userUserService.getLoggedUser(customUserDetails.getId());
     }
-
-    @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
-        UserResponse userResponse = userService.getUserById(id);
-        return ResponseEntity.ok(userResponse);
-    }
-
 }
-
