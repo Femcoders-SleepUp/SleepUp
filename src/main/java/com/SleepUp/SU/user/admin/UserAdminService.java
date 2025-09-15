cc83e2b5df4632dcdfee21648e9c182fa479d104
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-15 10:48:04 +0200

    refactor: rename EntityMapperUtil to EntityUtil

--- src/main/java/com/SleepUp/SU/user/admin/UserAdminService.java
+++ src/main/java/com/SleepUp/SU/user/admin/UserAdminService.java
@@ -6,7 +6,7 @@ import com.SleepUp.SU.user.UserRepository;
 import com.SleepUp.SU.user.utils.UserServiceHelper;
 import com.SleepUp.SU.user.dto.UserMapper;
 import com.SleepUp.SU.user.dto.UserResponse;
-import com.SleepUp.SU.utils.EntityMapperUtil;
+import com.SleepUp.SU.utils.EntityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,7 +21,7 @@ public class UserAdminService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final EntityMapperUtil mapperUtil;
+    private final EntityUtil mapperUtil;
     private final UserServiceHelper userServiceHelper;
 
 
