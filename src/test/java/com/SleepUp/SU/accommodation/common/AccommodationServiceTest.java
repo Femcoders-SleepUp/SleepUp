1ad42bb05a32f58e79de6362823976a143b45997
thaisrqueiroz <thaisrochadequeiroz@gmail.com>
2025-09-16 11:28:09 +0200

    test: add getAccommodations_shouldReturnListOfAccommodations, getAccommodationById_shouldReturnAccommodationDetail, getAllAccommodations_success and getAccommodationById_success tests

--- src/test/java/com/SleepUp/SU/accommodation/common/AccommodationServiceTest.java
+++ src/test/java/com/SleepUp/SU/accommodation/common/AccommodationServiceTest.java
@@ -5,6 +5,7 @@ import com.SleepUp.SU.accommodation.AccommodationRepository;
 import com.SleepUp.SU.accommodation.dto.AccommodationMapper;
 import com.SleepUp.SU.accommodation.dto.AccommodationRequest;
 import com.SleepUp.SU.accommodation.dto.AccommodationResponseDetail;
+import com.SleepUp.SU.accommodation.dto.AccommodationResponseSummary;
 import com.SleepUp.SU.user.User;
 import com.SleepUp.SU.utils.EntityUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,8 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -119,6 +122,28 @@ class AccommodationServiceTest {
                 null,
                 "new-image.jpg"
         );
+    }
+
+    @Test
+    void getAllAccommodations_success() {
+        when(accommodationRepository.findAll()).thenReturn(java.util.List.of(existingAccommodation));
+
+        List<AccommodationResponseSummary> result = accommodationService.getAllAccommodations();
+
+        assertThat(result).hasSize(1);
+        verify(accommodationRepository).findAll();
+    }
+
+    @Test
+    void getAccommodationById_success() {
+        when(accommodationRepository.findById(1L)).thenReturn(Optional.of(existingAccommodation));
+        when(accommodationMapper.toDetail(existingAccommodation)).thenReturn(updatedAccommodationResponseDetail);
+
+        AccommodationResponseDetail result = accommodationService.getAccommodationById(1L);
+
+        assertEquals(updatedAccommodationResponseDetail, result);
+        verify(accommodationRepository).findById(1L);
+        verify(accommodationMapper).toDetail(existingAccommodation);
     }
 
     @Test
