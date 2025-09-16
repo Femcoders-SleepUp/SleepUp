fa97c84cd09cc75773e668ca430d8c2d9a26397c
Bruna Sonda <bsonda@gmail.com>
2025-09-16 18:16:25 +0200

    feat(reservationrepository): add repository interface

---
+++ src/main/java/com/SleepUp/SU/reservation/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.SleepUp.SU.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long>, JpaSpecificationExecutor<Reservation> {
+}
