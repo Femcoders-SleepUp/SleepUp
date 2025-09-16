24f8b92dec6de3f6e6ac07a971da677ccf956451
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-16 20:37:38 +0200

    feat(reservation-mapper): create ReservationMapper; add toSummary, toDetail and toEntity methods

--- src/main/java/com/SleepUp/SU/reservation/dto/ReservationMapper.java
+++ src/main/java/com/SleepUp/SU/reservation/dto/ReservationMapper.java
@@ -1,4 +1,12 @@
 package com.SleepUp.SU.reservation.dto;
 
+import com.SleepUp.SU.reservation.Reservation;
+import com.SleepUp.SU.reservation.status.BookingStatus;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ReservationMapper {
+    ReservationResponseSummary toSummary(Reservation reservation);
+    ReservationResponseDetail toDetail(Reservation reservation);
+    Reservation toEntity(ReservationRequest reservationRequest, BookingStatus bookingStatus);
 }
