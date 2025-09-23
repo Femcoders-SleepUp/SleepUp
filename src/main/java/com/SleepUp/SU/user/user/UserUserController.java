9475033d2bcd90e8af3480d8c91582a291ce4dae
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-23 14:15:35 +0200

    feat(user-user-security): add controller paths to requestMatchers in security config

--- src/main/java/com/SleepUp/SU/user/user/UserUserController.java
+++ src/main/java/com/SleepUp/SU/user/user/UserUserController.java
@@ -7,11 +7,13 @@ import com.SleepUp.SU.utils.ApiMessageDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users/my-user")
+@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 public class UserUserController {
 
