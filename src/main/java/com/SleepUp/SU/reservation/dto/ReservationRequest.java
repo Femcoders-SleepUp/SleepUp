bcad4847e9d4060bd56c229fc5a53efc4a8864db
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-09-25 12:40:18 +0200

    refactor(reservation-dto): add jason format annotation to date fields in request

--- src/main/java/com/SleepUp/SU/reservation/dto/ReservationRequest.java
+++ src/main/java/com/SleepUp/SU/reservation/dto/ReservationRequest.java
@@ -1,5 +1,6 @@
 package com.SleepUp.SU.reservation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
@@ -9,9 +10,11 @@ public record ReservationRequest(
         Integer guestNumber,
 
         @NotNull
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate checkInDate,
 
         @NotNull
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate checkOutDate
 ) {
 }
