eb41d516545e5de7bf7fea4b925572ea42fa7864
Bruna Sonda <bsonda@gmail.com>
2025-09-29 14:01:05 +0200

    feat(reservation-admin): add getallreservations method

--- src/main/java/com/SleepUp/SU/reservation/admin/ReservationAdminService.java
+++ src/main/java/com/SleepUp/SU/reservation/admin/ReservationAdminService.java
@@ -1,6 +1,10 @@
 package com.SleepUp.SU.reservation.admin;
 
 
+import com.SleepUp.SU.reservation.dto.ReservationResponseSummary;
+
+import java.util.List;
 
 public interface ReservationAdminService {
+    List<ReservationResponseSummary> getAllReservations();
 }
