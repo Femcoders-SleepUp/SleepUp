092d03ddcbcd99ac325672d929ef92e44c41ccca
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-09-30 15:50:05 +0200

    chore(accommodation-controller): enhance filter accommodation API documentation with Swagger annotations

--- src/main/java/com/SleepUp/SU/accommodation/filter/AccommodationFilterController.java
+++ src/main/java/com/SleepUp/SU/accommodation/filter/AccommodationFilterController.java
@@ -2,6 +2,9 @@ package com.SleepUp.SU.accommodation.filter;
 
 import com.SleepUp.SU.accommodation.dto.AccommodationResponseSummary;
 import com.SleepUp.SU.accommodation.dto.FilterAccommodationDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Accommodation Filter", description = "Operations related to filtering accommodations")
 @RestController
 @RequestMapping("/accommodations")
 @RequiredArgsConstructor
@@ -18,6 +22,12 @@ public class AccommodationFilterController {
     private final AccommodationFilterService accommodationFilterService;
 
     @GetMapping("/filter")
+    @Operation(summary = "Filter Accommodations", description = "Retrieve a paginated list of accommodations based on filter criteria.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved filtered accommodations"),
+                    @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
+                    @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+            })
     @ResponseStatus(HttpStatus.OK)
     public Page<AccommodationResponseSummary> getAllFilteredAccommodationsWithPagination(
             @ModelAttribute @Valid FilterAccommodationDTO filter,
