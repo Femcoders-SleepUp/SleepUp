418ba9fe6ff230922c671cec88a6fad0246196ed
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-09-30 15:50:52 +0200

    chore(accommodation-controller): enhance owner accommodation API documentation with Swagger annotations

--- src/main/java/com/SleepUp/SU/accommodation/owner/AccommodationOwnerController.java
+++ src/main/java/com/SleepUp/SU/accommodation/owner/AccommodationOwnerController.java
@@ -2,6 +2,9 @@ package com.SleepUp.SU.accommodation.owner;
 
 import com.SleepUp.SU.accommodation.dto.AccommodationResponseSummary;
 import com.SleepUp.SU.user.entity.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Accommodation Owner", description = "Operations related to accommodations by owners")
 @RestController
 @RequestMapping("/accommodations")
 @RequiredArgsConstructor
@@ -21,6 +25,13 @@ public class AccommodationOwnerController {
     private final AccommodationOwnerService accommodationOwnerService;
 
     @GetMapping("/me")
+    @Operation(summary = "Get All My Accommodations", description = "Retrieve a list of accommodations owned by the authenticated user.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of accommodations"),
+                    @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
+                    @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+                    @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+            })
     @ResponseStatus(HttpStatus.OK)
     public List<AccommodationResponseSummary> getAllOwnerAccommodations(@AuthenticationPrincipal CustomUserDetails customUserDetails){
         return accommodationOwnerService.getAllAccommodationsByOwnerId(customUserDetails.getId());
