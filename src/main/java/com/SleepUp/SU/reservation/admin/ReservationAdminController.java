f3702f8674c0429c0e79ec6c4f66cfd1426fac5a
Thais Rocha <thaisrochadequeiroz@gmail.com>
2025-10-06 10:00:28 +0200

    chore(reservation-admin-controller): enhance accommodation API documentation with Swagger annotations

--- src/main/java/com/SleepUp/SU/reservation/admin/ReservationAdminController.java
+++ src/main/java/com/SleepUp/SU/reservation/admin/ReservationAdminController.java
@@ -1,10 +1,11 @@
 package com.SleepUp.SU.reservation.admin;
 
-
 import com.SleepUp.SU.reservation.dto.ReservationResponseSummary;
 import com.SleepUp.SU.reservation.entity.Reservation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Admin Reservation", description = "Operations related to reservations by admin")
 @RestController
 @RequestMapping("/reservations/admin")
 @RequiredArgsConstructor
@@ -21,12 +23,27 @@ public class ReservationAdminController {
     private final ReservationAdminServiceImpl reservationAdminService;
 
     @GetMapping
+    @Operation(summary = "Get All Reservations", description = "Retrieve all reservations in the system. Accessible only by admin users.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of all reservations"),
+                    @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+                    @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+            })
     @ResponseStatus(HttpStatus.OK)
     public List<ReservationResponseSummary> getAllReservations(@AuthenticationPrincipal Reservation reservation){
         return reservationAdminService.getAllReservations();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Reservation", description = "Delete a reservation by its ID. Accessible only by admin users.",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Successfully deleted the reservation"),
+                    @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
+                    @ApiResponse(responseCode = "401", ref = "#/components/responses/Unauthorized"),
+                    @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+                    @ApiResponse(responseCode = "404", ref = "#/components/responses/ReservationFound"),
+                    @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+            })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteReservationByAdmin(@PathVariable Long id) {
         reservationAdminService.deleteReservationByAdmin(id);
