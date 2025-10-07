fc226b6893dd0f47eb57ddfec4b1a3a57ce01db9
Bruna Sonda <bsonda@gmail.com>
2025-10-07 10:17:43 +0200

    refactor(reservation): simplify if/else logic in validateAccomodationAvailability, remove ReservationRequest from AccommodationUnavailableException, fix test

--- src/main/java/com/SleepUp/SU/reservation/exceptions/AccommodationUnavailableException.java
+++ src/main/java/com/SleepUp/SU/reservation/exceptions/AccommodationUnavailableException.java
@@ -5,7 +5,7 @@ import com.SleepUp.SU.exceptions.AppException;
 import com.SleepUp.SU.reservation.dto.ReservationRequest;
 
 public class AccommodationUnavailableException extends AppException {
-    public AccommodationUnavailableException(Accommodation accommodation, ReservationRequest reservationRequest) {
+    public AccommodationUnavailableException(Accommodation accommodation) {
         super(String.format("Accommodation is only available from %s to %s", accommodation.getAvailableFrom(), accommodation.getAvailableTo()));
     }
 }
