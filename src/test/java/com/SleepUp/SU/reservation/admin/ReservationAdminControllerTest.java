ed48f7b199606cf47a049772cecdb0a475d93e6e
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-10-07 10:29:56 +0200

    test(reservation-admin-controller): add tests for getAllReservations

--- src/test/java/com/SleepUp/SU/reservation/admin/ReservationAdminControllerTest.java
+++ src/test/java/com/SleepUp/SU/reservation/admin/ReservationAdminControllerTest.java
@@ -1,14 +1,12 @@
 package com.SleepUp.SU.reservation.admin;
 
-import com.SleepUp.SU.accommodation.repository.AccommodationRepository;
-import com.SleepUp.SU.reservation.controller.ReservationController;
+import com.SleepUp.SU.reservation.entity.Reservation;
 import com.SleepUp.SU.reservation.repository.ReservationRepository;
 import com.SleepUp.SU.reservation.service.ReservationServiceImpl;
 import com.SleepUp.SU.user.admin.UserAdminServiceImpl;
 import com.SleepUp.SU.user.entity.CustomUserDetails;
 import com.SleepUp.SU.user.entity.User;
 import com.SleepUp.SU.user.repository.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -35,8 +34,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 public class ReservationAdminControllerTest {
 
-    private static final String RESERVATIONS_PATH = "/reservations";
-    private static final String ACCOMMODATIONS_PATH = "/accommodations";
+    private static final String RESERVATIONS_ADMIN_PATH = "/reservations/admin";
+    private static final String RESERVATIONS_ADMIN_PATH_ID = "/reservations/admin/{id}";
 
     @Autowired
     private WebApplicationContext context;
@@ -48,9 +47,6 @@ public class ReservationAdminControllerTest {
     private UserAdminServiceImpl userAdminServiceImpl;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
     private UserRepository userRepository;
 
     @Autowired
@@ -58,12 +54,6 @@ public class ReservationAdminControllerTest {
 
     @Autowired
     private ReservationServiceImpl reservationServiceImpl;
-
-    @Autowired
-    private ReservationController reservationController;
-
-    @Autowired
-    private AccommodationRepository accommodationRepository;
 
     private CustomUserDetails principal;
 
@@ -80,6 +70,38 @@ public class ReservationAdminControllerTest {
         principal = new CustomUserDetails(savedUser);
     }
 
+    @Nested
+    class GetAllReservationsAdminTests {
+
+        @Test
+        void getAllReservations_asAdmin_shouldReturnNoContent() throws  Exception {
+            Reservation reservation = reservationRepository.findById(1L).orElseThrow(() -> new RuntimeException("Reservation not found"));
+
+            mockMvc.perform(get(RESERVATIONS_ADMIN_PATH)
+                            .with(user("admin").roles("ADMIN"))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasSize(5)))
+                    .andExpect(jsonPath("$[0].id").value(reservation.getId()))
+                    .andExpect(jsonPath("$[0].userName").value(reservation.getUser().getName()))
+                    .andExpect(jsonPath("$[0].guestNumber").value(reservation.getGuestNumber()))
+                    .andExpect(jsonPath("$[0].accommodationName").value(reservation.getAccommodation().getName()))
+                    .andExpect(jsonPath("$[0].checkInDate").value(reservation.getCheckInDate().toString()))
+                    .andExpect(jsonPath("$[0].checkOutDate").value(reservation.getCheckOutDate().toString()))
+                    .andExpect(jsonPath("$[0].bookingStatus").value(reservation.getBookingStatus().toString()))
+                    .andExpect(jsonPath("$[0].totalPrice").value(reservation.getTotalPrice().doubleValue()));
+        }
+
+        @Test
+        void getAllReservations_nonAdmin_shouldReturnForbidden() throws Exception {
+            mockMvc.perform(get(RESERVATIONS_ADMIN_PATH)
+                            .with(user(principal))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+    }
 
     @Nested
     class DeleteReservationAdminTests {
@@ -88,7 +110,7 @@ public class ReservationAdminControllerTest {
         void deleteReservation_asAdmin_shouldReturnNoContent() throws  Exception {
             Long reservationId = 3L;
 
-            mockMvc.perform(delete(RESERVATIONS_PATH + "/admin/{id}", reservationId)
+            mockMvc.perform(delete(RESERVATIONS_ADMIN_PATH_ID, reservationId)
                             .with(user("admin").roles("ADMIN"))
                             .contentType(MediaType.APPLICATION_JSON))
                     .andDo(print())
@@ -99,8 +121,8 @@ public class ReservationAdminControllerTest {
         void deleteReservation_nonAdmin_shouldReturnForbidden() throws Exception {
             Long reservationId = 3L;
 
-            mockMvc.perform(delete(RESERVATIONS_PATH + "/admin/{id}", reservationId)
-                            .with(user(principal)) // usuario normal
+            mockMvc.perform(delete(RESERVATIONS_ADMIN_PATH_ID, reservationId)
+                            .with(user(principal))
                             .contentType(MediaType.APPLICATION_JSON))
                     .andDo(print())
                     .andExpect(status().isForbidden());
@@ -110,7 +132,7 @@ public class ReservationAdminControllerTest {
         void deleteReservation_notFound_shouldReturnNotFound() throws Exception {
             Long reservationId = 999L;
 
-            mockMvc.perform(delete(RESERVATIONS_PATH + "/admin/{id}", reservationId)
+            mockMvc.perform(delete(RESERVATIONS_ADMIN_PATH_ID, reservationId)
                             .with(user("admin").roles("ADMIN"))
                             .contentType(MediaType.APPLICATION_JSON))
                     .andDo(print())
